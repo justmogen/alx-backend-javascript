8cba35f7022c13dfb481127c3148a6f6b0bf63c9
justmogen <justmogen@gmail.com>
2023-09-28 11:47:14 +0300

    adding 'Guardrail.. for both success and error outputs

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -7,12 +7,10 @@ export default async function handleProfileSignup(firstName, lastName, fileName)
     const user = await signUpUser(firstName, lastName);
     promises.push({ status: 'fulfilled', value: user });
     await uploadPhoto(fileName);
-    promises.push('Guardrail was processed');
   } catch (err) {
     promises.push({
       status: 'rejected',
       value: `Error: ${fileName} cannot be processed`,
     });
-    promises.push('Guardrail was processed');
   }
 }
