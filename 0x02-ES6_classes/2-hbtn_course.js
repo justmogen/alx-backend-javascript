6e1b360d94effb9c52bc47b42f1914c4e61c61e3
justmogen <justmogen@gmail.com>
2023-09-28 16:04:33 +0300

    A course, Getters and Setters

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,48 @@
+class HolbertonCourse {
+    constructor(name, length, students) {
+        // Verify the type of the attributes
+        if (typeof name !== 'string' || typeof length !== 'number' || !Array.isArray(students)) {
+            throw new TypeError('Invalid attribute types');
+        }
+       
+
+        this._name = name;
+        this._length = length;
+        this.students = students;
+    }
+
+    //Getter and setter for name
+    get name() {
+        return this._name;
+    }
+    set name(newName) {
+        if(typeof newName !== 'string'){
+            throw new TypeError('Name must be a string');
+        }
+        this._name = newName;
+    }
+
+    //Getter and setter for length
+    get length() {
+        return this._length;
+    }
+    set length(newLength) {
+        if(typeof newLength !== 'number'){
+            throw new TypeError('Length must be a number');
+        }
+        this._length = newLength;
+    }
+
+    //Getter and setter for students
+    get students() {
+        return this._students;
+    }
+    set students(newStudents) {
+        if (!Array.isArray(newStudents)) {
+            throw new TypeError('Students must be an array');
+        }
+        this._students = newStudents;
+    }
+}
+
+export default HolbertonCourse;
