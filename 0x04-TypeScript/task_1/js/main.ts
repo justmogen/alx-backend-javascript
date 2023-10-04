a143be8614f64f127ccd31c05c5e965ac837fdf0
justmogen <justmogen@gmail.com>
2023-10-04 17:33:01 +0300

    task 1 TS codebase

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -6,3 +6,45 @@ interface Teacher {
     location: string;
     [key: string]: any; // Index signature for dynamic properties
 }
+
+interface Directors extends Teacher {
+    numberOfReports: number;
+}
+
+interface printTeacherFunction {
+    (firstName: string, lastName: string): string;
+}
+
+export const printTeacher: printTeacherFunction = (firstName: string, lastName: string): string => {
+    return `${firstName.charAt(0)}. ${lastName}`;
+}
+
+interface StudentConstructor {
+    firstName: string;
+    lastName: string;
+}
+
+interface StudentClassInterface {
+    workOnHomework(): string;
+    displayName(): string;
+}
+
+class StudentClass implements StudentClassInterface{
+    private readonly firstName: string;
+    private readonly lastName: string;
+
+    constructor({firstName, lastName}: StudentConstructor) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    workOnHomework(): string {
+        return 'Currently working';
+    }
+
+    displayName(): string {
+        return this.firstName;
+    }
+}
+
+export default StudentClass
