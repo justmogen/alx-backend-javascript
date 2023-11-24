be364edeba7acc46b77b689b2f25fe0f051dca7e
justmogen <justmogen@gmail.com>
2023-11-24 17:44:03 +0300

    Basic test with Mocha and Node assertion library

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,9 +1,7 @@
-module.exports = function calculateNumber(a, b = 0) {
-  const aNum = Number(a);
-  const bNum = Number(b);
+// 0-calcul.js
+function calculateNumber(a, b) {
+  return Math.round(a) + Math.round(b);
+}
 
-  if (Number.isNaN(aNum) || Number.isNaN(bNum))
-    throw TypeError('Parameters must be numbers');
+module.exports = calculateNumber;
 
-  return Math.round(aNum) + Math.round(bNum);
-};
